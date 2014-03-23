ba5a3699b2b3472e6e06f4a8539a37c1ff596ad2
Sumanas Sarma <insectatorious+git@gmail.com>
2014-03-23 23:55:24 +0000

    Updated winning tweet message.

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -155,13 +155,12 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   var tweet = document.createElement("a");
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
+  tweet.setAttribute("data-via", "qaoqz");
+  tweet.setAttribute("data-url", "http://git.io/tickets2048");
+  tweet.setAttribute("data-counturl", "http://insectatorious.github.io/2048/");
   tweet.textContent = "Tweet";
 
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
+  var text = "I scored " + this.score + " points at Tickets! Cloned from #2048game";
   tweet.setAttribute("data-text", text);
 
   return tweet;
