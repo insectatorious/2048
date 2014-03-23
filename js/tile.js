c7c221ae26f74f33ddaf601f4d5a124db2703f34
Sumanas Sarma <insectatorious@gmail.com>
2014-03-23 11:35:00 +0000

    Update tile.js

--- js/tile.js
+++ js/tile.js
@@ -1,7 +1,21 @@
+devStages = {
+  2:  "New",
+  4:  "Research",
+  8:  "Buss. Rev.",
+  16: "3rd Party",
+  32: "Implementation",
+  64: "Code Review",
+  128: "QA",
+  512: "Deployment",
+  1024: "Prod Follow-up",
+  2048: "Resolved"
+};
+
 function Tile(position, value) {
   this.x                = position.x;
   this.y                = position.y;
   this.value            = value || 2;
+  this.text             = devStages[value];
 
   this.previousPosition = null;
   this.mergedFrom       = null; // Tracks tiles that merged together
@@ -23,5 +37,7 @@ Tile.prototype.serialize = function () {
       y: this.y
     },
     value: this.value
+    text:  this.text
   };
 };
+
